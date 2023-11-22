067edfb5556ac239c730e5890217fd871595edcc
teodoracara21 <carateodora21@stud.ase.ro>
2023-11-22 23:40:10 +0200

    added classes

---
+++ OOP_project/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Location {
+private:
+    int NoOfRows;
+    char NameOfLocation;
+    const int MaxSeats;
+    const int MaxRows;
+
+public:
+
+};
+
+class Event {
+private:
+    char Date;
+    char Time;
+    char NameOfEvent;
+public:
+
+};
+
+class Details {
+    char Type;
+
+};
